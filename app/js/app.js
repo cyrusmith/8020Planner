c4a36244390a4092dfd166f38887b32b95ed24b0
cyrusmith <info@interosite.ru>
2014-07-19 20:02:49 +0600

    Sortable based on angular UI

--- app/js/app.js
+++ app/js/app.js
@@ -2,14 +2,15 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'MainController'});
-  $routeProvider.otherwise({redirectTo: '/'});
-}]);
+angular.module('8020Planner', [
+        'ngRoute',
+        '8020Planner.filters',
+        '8020Planner.services',
+        '8020Planner.directives',
+        '8020Planner.controllers',
+        'ui.sortable'
+    ]).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'MainController'});
+        $routeProvider.otherwise({redirectTo: '/'});
+    }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,89 +2,65 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-    .controller('MainController', ['$scope', function ($scope) {
+angular.module('8020Planner.controllers', [])
+    .controller('MainController', ['$scope', '$filter', function ($scope, $filter) {
 
-        function Task(id, title) {
+        function Task(id, title, state) {
             this.id = id;
             this.title = title;
-            this.isToday = false;
-            this.isComplete = false;
+            this.state = state;
         }
 
-        Task.prototype.setToday = function (isToday) {
-            this.isToday = true;
-            return this;
-        }
+        Task.STATE_BACKLOG = 1;
+        Task.STATE_TODO = 2;
+        Task.STATE_COMPLETE = 3;
 
-        Task.prototype.setComplete = function () {
-            if (this.isToday) {
-                this.isComplete = true;
-            }
-            return this;
-        }
+        var sortableOptionsCommon = {
+            placeholder: "task-ph",
+            start: function(event, ui){
 
-        function TaskList() {
-            this.__hash = {};
-            this.__items = [];
-        }
-
-        TaskList.prototype.append = function (todo) {
-            if (!(todo instanceof Task)) throw "Illegal argument";
-            if (!this.__hash.hasOwnProperty(todo.id)) {
-                this.__hash[todo.id] = todo;
-                this.__items.push(todo);
-            }
-        }
-
-        TaskList.prototype.getTasks = function () {
-            return this.__items;
-        }
-
-        TaskList.prototype.update = function (tasks) {
-
-            this.__hash = {};
-            this.__items = [];
-
-            for (var i = 0; i < tasks.length; i++) {
-                this.__hash[tasks[i].id] = tasks[i];
-                this.__items.push(tasks[i]);
-            }
-        }
-
-        $scope.tasks = {};
-        $scope.tasks["1"] = new Task("1", "Todo 1");
-        $scope.tasks["2"] = new Task("2", "Todo 2");
-        $scope.tasks["3"] = new Task("3", "Todo 3").setToday(true);
-
-        $scope.backlog = new TaskList();
-        $scope.todays = new TaskList();
-
-        function configBoard() {
-            for (var id in $scope.tasks) {
-                if ($scope.tasks.hasOwnProperty(id)) {
-                    $scope.tasks[id].isToday ? $scope.todays.append($scope.tasks[id]) : $scope.backlog.append($scope.tasks[id]);
+            },
+            beforeStop: function (evt, ui) {
+                var id = ui.helper[0].id;
+                var task = $.grep($scope.tasks, function (item) {
+                    return item.id == id;
+                })[0];
+                if (!task || task === undefined) {
+                    return;
+                }
+                var typeString = ui.helper.parent()[0].getAttribute('data-taskstate');
+                if (task.state != typeString) {
+                    task.state = parseInt(typeString);
                 }
             }
-        }
+        };
 
-        configBoard();
+        $scope.states = {
+            "backlog": Task.STATE_BACKLOG,
+            "todo": Task.STATE_TODO,
+            "complete": Task.STATE_COMPLETE
+        };
 
-        $scope.onUpdateBacklog = function (ids) {
-            console.log("onUpdateBacklog", ids);
-            var tasks = [];
-            for (var i = 0; i < ids.length; i++) {
-                tasks.push($scope.tasks[ids[i]].setToday(false));
-            }
-            $scope.backlog.update(tasks);
-        }
+        $scope.tasks = [];
+        $scope.tasks.push(new Task(1, "Save the world", Task.STATE_BACKLOG));
+        $scope.tasks.push(new Task(2, "Buy the bread", Task.STATE_BACKLOG));
+        $scope.tasks.push(new Task(3, "Kill Bill", Task.STATE_BACKLOG));
 
-        $scope.onUpdateToday = function (ids) {
-            console.log("onUpdateToday", ids);
-            var tasks = [];
-            for (var i = 0; i < ids.length; i++) {
-                tasks.push($scope.tasks[ids[i]].setToday(true));
-            }
-            $scope.todays.update(tasks);
-        }
+        $scope.backlog = $filter('filter')($scope.tasks, {
+            state: Task.STATE_BACKLOG
+        });
+
+        $scope.todo = $filter('filter')($scope.tasks, {
+            state: Task.STATE_TODO
+        });
+
+        $scope.sortableOptionsBacklog = angular.extend(sortableOptionsCommon, {
+            connectWith: '.task-list'
+        });
+
+        $scope.sortableOptionsTodo = angular.extend(sortableOptionsCommon, {
+            connectWith: '.task-list'
+        });
+
+
     }]);
